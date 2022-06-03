620365bf0cebfa57621ef971b793abb3e6e9ee2f
Maxim <Gazaf@mail.ru>
2022-06-03 18:39:58 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,47 +1,47 @@
-const url = 'https://api.github.com/search/repositories';
-const field = document.querySelector('.input__field');
-const autocomplete = document.querySelector('.input__autocomplete');
-const results = document.querySelector('.results');
+const url = "https://api.github.com/search/repositories";
+const field = document.querySelector(".input__field");
+const autocomplete = document.querySelector(".input__autocomplete");
+const results = document.querySelector(".results");
 
 function debounce(fn, ms) {
   let inDebounce;
 
   return function (...args) {
-    return new Promise(res => {
+    return new Promise((resolve) => {
       clearTimeout(inDebounce);
-      inDebounce = setTimeout(() => res(fn.apply(this, args)), ms);
+      inDebounce = setTimeout(() => resolve(fn.apply(this, args)), ms);
     });
   };
 }
 
 function createAutoCompleteElement(data) {
-  const li = document.createElement('li');
-  li.textContent = data.name;
-  li.dataset.id = data.id;
-  li.dataset.owner = data.owner.login;
-  li.dataset.stars = data.stargazers_count;
+  const list = document.createElement("li");
+  list.textContent = data.name;
+  list.dataset.id = data.id;
+  list.dataset.owner = data.owner.login;
+  list.dataset.stars = data.stargazers_count;
 
-  autocomplete.append(li);
-  autocomplete.addEventListener('click', renderResults);
+  autocomplete.append(list);
+  autocomplete.addEventListener("click", renderResults);
 }
 
 function fillAutoComplete(data) {
-  autocomplete.innerHTML = '';
+  autocomplete.innerHTML = "";
 
   if (data) {
-    if (!data.total_count) autocomplete.textContent = 'Ничего не найдено!';
+    if (!data.total_count) autocomplete.textContent = "Ничего не найдено!";
     data.items.forEach(createAutoCompleteElement);
   }
 }
 
-function renderResults(e) {
-  e.target.remove();
+function renderResults(element) {
+  element.target.remove();
 
   results.innerHTML += `<li class="results__item">
   <div class="results__block">
-    <p class="results__name">Name: ${e.target.textContent}</p>
-    <p class="results__owner">Owner: ${e.target.dataset.owner}</p>
-    <p class="results__stars">Starts: ${e.target.dataset.stars}</p>
+    <p class="results__name">Name: ${element.target.textContent}</p>
+    <p class="results__owner">Owner: ${element.target.dataset.owner}</p>
+    <p class="results__stars">Starts: ${element.target.dataset.stars}</p>
   </div>
   <div class="button">
     <span class="button__left"></span>
@@ -49,44 +49,44 @@ function renderResults(e) {
   </div>
   </li>`;
 
-  results.addEventListener('click', onRemoveResult);
+  results.addEventListener("click", onRemoveResult);
 
   if (!autocomplete.children.length) {
-    field.value = '';
-    autocomplete.removeEventListener('click', renderResults);
+    field.value = "";
+    autocomplete.removeEventListener("click", renderResults);
   }
 }
 
-function onRemoveResult(e) {
-  if (e.target.classList.contains('button')) {
-    e.target.parentElement.remove();
+function onRemoveResult(element) {
+  if (element.target.classList.contains("button")) {
+    element.target.parentElement.remove();
   }
 
   if (
-    e.target.classList.contains('button__left') ||
-    e.target.classList.contains('button__right')
+    element.target.classList.contains("button__left") ||
+    element.target.classList.contains("button__right")
   ) {
-    e.target.parentElement.parentElement.remove();
+    element.target.parentElement.parentElement.remove();
   }
 
   if (!results.children.length)
-    results.removeEventListener('click', onRemoveResult);
+    results.removeEventListener("click", onRemoveResult);
 }
 
 async function getRepos(query) {
   if (!field.value || !query.trim()) {
-    autocomplete.innerHTML = '';
-    autocomplete.removeEventListener('click', renderResults);
+    autocomplete.innerHTML = "";
+    autocomplete.removeEventListener("click", renderResults);
     return;
   }
 
   return await fetch(`${url}?q=${query}&per_page=5`)
-    .then(res => (res.ok ? res.json() : Promise.reject(res)))
-    .catch(e => console.log(e));
+    .then((response) => (response.ok ? response.json() : Promise.reject(res)))
+    .catch((e) => console.log(e));
 }
 
 const debouncedGetRepos = debounce(getRepos, 500);
 
-field.addEventListener('input', e =>
-  debouncedGetRepos(e.target.value).then(fillAutoComplete)
+field.addEventListener("input", (event) =>
+  debouncedGetRepos(event.target.value).then(fillAutoComplete)
 );
